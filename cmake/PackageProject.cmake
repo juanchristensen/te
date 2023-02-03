31775f6b9ba25758468bb7f1465cd5eb3521688d
Juan Carlos Christensen <juan@mundi.io>
2023-02-03 11:15:35 -0300

    Updated PackageProject.cmake

--- cmake/PackageProject.cmake
+++ cmake/PackageProject.cmake
@@ -1,26 +1,4 @@
-# ------------------------------------------------------------------------------
-# MIT License
-#
-# Copyright (c) 2020 Lars Melchior
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-# ------------------------------------------------------------------------------
+cmake_minimum_required(VERSION 3.14)
 
 set(PACKAGE_PROJECT_ROOT_PATH
         "${CMAKE_CURRENT_LIST_DIR}"
@@ -65,7 +43,9 @@ function(packageProject)
 
         if(DEFINED PROJECT_EXPORT_HEADER)
             include(GenerateExportHeader)
-            generate_export_header(${PROJECT_NAME} EXPORT_FILE_NAME ${PROJECT_VERSION_INCLUDE_DIR}/${PROJECT_EXPORT_HEADER})
+            generate_export_header(
+                    ${PROJECT_NAME} EXPORT_FILE_NAME ${PROJECT_VERSION_INCLUDE_DIR}/${PROJECT_EXPORT_HEADER}
+            )
         endif()
 
         if(DEFINED PROJECT_VERSION_HEADER)
@@ -75,7 +55,9 @@ function(packageProject)
             unset(CMAKE_MATCH_5)
             unset(CMAKE_MATCH_7)
 
-            string(REGEX MATCH "^([0-9]+)(\\.([0-9]+))?(\\.([0-9]+))?(\\.([0-9]+))?$" _ "${PROJECT_VERSION}")
+            string(REGEX MATCH "^([0-9]+)(\\.([0-9]+))?(\\.([0-9]+))?(\\.([0-9]+))?$" _
+                    "${PROJECT_VERSION}"
+                    )
 
             set(PROJECT_VERSION_MAJOR ${CMAKE_MATCH_1})
             set(PROJECT_VERSION_MINOR ${CMAKE_MATCH_3})
@@ -96,7 +78,10 @@ function(packageProject)
             endif()
 
             string(TOUPPER ${PROJECT_NAME} UPPERCASE_PROJECT_NAME)
-            configure_file(${PACKAGE_PROJECT_ROOT_PATH}/version.h.in ${PROJECT_VERSION_INCLUDE_DIR}/${PROJECT_VERSION_HEADER} @ONLY)
+            configure_file(
+                    ${PACKAGE_PROJECT_ROOT_PATH}/version.h.in
+                    ${PROJECT_VERSION_INCLUDE_DIR}/${PROJECT_VERSION_HEADER} @ONLY
+            )
         endif()
 
         get_target_property(target_type ${PROJECT_NAME} TYPE)
@@ -105,7 +90,9 @@ function(packageProject)
         else()
             set(VISIBILITY PUBLIC)
         endif()
-        target_include_directories(${PROJECT_NAME} ${VISIBILITY} "$<BUILD_INTERFACE:${PROJECT_VERSION_INCLUDE_DIR}>")
+        target_include_directories(
+                ${PROJECT_NAME} ${VISIBILITY} "$<BUILD_INTERFACE:${PROJECT_VERSION_INCLUDE_DIR}>"
+        )
         install(
                 DIRECTORY ${PROJECT_VERSION_INCLUDE_DIR}/
                 DESTINATION ${PROJECT_INCLUDE_DESTINATION}
@@ -137,9 +124,12 @@ function(packageProject)
             LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
             COMPONENT "${PROJECT_NAME}_Runtime"
             NAMELINK_COMPONENT "${PROJECT_NAME}_Development"
-            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT "${PROJECT_NAME}_Development"
-            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT "${PROJECT_NAME}_Runtime"
-            BUNDLE DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX} COMPONENT "${PROJECT_NAME}_Runtime"
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
+            COMPONENT "${PROJECT_NAME}_Development"
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
+            COMPONENT "${PROJECT_NAME}_Runtime"
+            BUNDLE DESTINATION ${CMAKE_INSTALL_BINDIR}/${PROJECT_NAME}${PROJECT_VERSION_SUFFIX}
+            COMPONENT "${PROJECT_NAME}_Runtime"
             PUBLIC_HEADER DESTINATION ${PROJECT_INCLUDE_DESTINATION} COMPONENT "${PROJECT_NAME}_Development"
             INCLUDES
             DESTINATION "${PROJECT_INCLUDE_DESTINATION}"
@@ -152,7 +142,10 @@ function(packageProject)
 
     mark_as_advanced("${PROJECT_NAME}_INSTALL_CMAKEDIR")
 
-    configure_file(${PACKAGE_PROJECT_ROOT_PATH}/Config.cmake.in "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" @ONLY)
+    configure_file(
+            ${PACKAGE_PROJECT_ROOT_PATH}/Config.cmake.in
+            "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" @ONLY
+    )
 
     install(
             EXPORT ${PROJECT_NAME}Targets
@@ -162,7 +155,8 @@ function(packageProject)
     )
 
     install(
-            FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake" "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+            FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+            "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
             DESTINATION "${${PROJECT_NAME}_INSTALL_CMAKEDIR}"
             COMPONENT "${PROJECT_NAME}_Development"
     )
